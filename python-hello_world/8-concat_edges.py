dec2536175b01e5ec1eb57c9e72bf9fc08dca137
mammadow <mammadow.elnur@gmail.com>
2025-11-29 19:45:37 +0000

    fix

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-print(str[39:66] + str[106:112] + str[:6])
+str = "Python object-oriented programming with "
+print(str[7:35] + str[35:41] + str[:6])
