b6988a4a848e20b1c2c3daa1fdd156555f391e57
mammadow <mammadow.elnur@gmail.com>
2025-11-30 09:48:34 +0000

    fix

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    print("{}".format("".join(chr(i) if (i != 101 and i != 113) else "")), end="")
+    print("{}".format("".join(chr(i) if (i != 101 and i != 113)\
+ else "")), end="")
