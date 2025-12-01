4d8c48daf87ccb9ebc7b6bacd7acb512cd68886d
mammadow <mammadow.elnur@gmail.com>
2025-12-01 16:01:17 +0000

    fix

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-import add_0
-a = 1
-b = 2
-print(f"{a} + {b} = {add_0.add(a, b)}")
+from add_0 import add
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
