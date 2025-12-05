c363001852e714967ba4d700778db34214a9e9b7
mammadow <mammadow.elnur@gmail.com>
2025-12-05 12:19:39 +0000

    add new in list

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    list = my_list
+    if (idx < 0):
+        return list
+    if (idx >= len(list)):
+        return list
+    list[idx] = element
+    return list
