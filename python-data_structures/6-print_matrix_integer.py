418421c4728579e12b797c6a88223b02762c94c9
mammadow <mammadow.elnur@gmail.com>
2025-12-05 12:41:42 +0000

    fix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in range(len(i)):
-            if (i != 0):
+            if (j != 0):
                 print(" ", end="")
             print("{:d}".format(i[j]), end="")
         print()
