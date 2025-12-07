bcc265f02b2ad8d40dced100730752079917eeca
mammadow <mammadow.elnur@gmail.com>
2025-12-07 15:43:49 +0000

    add readme.md and square matrix simple

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new = []
+    for i in range(len(matrix)):
+        tlist = []
+        for j in range(len(matrix[i])):
+                tlist.append(matrix[i][j]**2)
+        new.append(tlist)
+    return new
