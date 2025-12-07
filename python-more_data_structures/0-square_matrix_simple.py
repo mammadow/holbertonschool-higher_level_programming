4306d1377b6c35c9e4299f10767e2de554032cbe
mammadow <mammadow.elnur@gmail.com>
2025-12-07 15:45:02 +0000

    fix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -4,6 +4,6 @@ def square_matrix_simple(matrix=[]):
     for i in range(len(matrix)):
         tlist = []
         for j in range(len(matrix[i])):
-                tlist.append(matrix[i][j]**2)
+            tlist.append(matrix[i][j]**2)
         new.append(tlist)
     return new
