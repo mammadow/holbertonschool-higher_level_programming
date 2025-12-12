1c25f033e8f56912cb05655fb8287d11b37ae7ad
mammadow <mammadow.elnur@gmail.com>
2025-12-12 11:35:01 +0000

    add rectangle repr

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""Defining a Rectangle class."""
+
+
+class Rectangle:
+    """
+    Defining a Rectangle class.
+    """
+
+    @property
+    def width(self):
+        """
+        Returns value of width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Set value of width
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        Returns value of height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Set value of height
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __init__(self, width=0, height=0):
+        """
+        Initializes a new square instance.
+        """
+
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        rect = []
+        for i in range(self.height):
+            rect.append("#" * self.__width)
+        return "\n".join(rect)
+
+    def __repr__(self):
+        return f"Rectangle({width}, {height})"
