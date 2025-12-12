420f1a23b2aca388aa23371b654a369b20445318
mammadow <mammadow.elnur@gmail.com>
2025-12-12 16:09:05 +0000

    improve base geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Defines a class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    Base class for geometry objects.
+    """
+
+    def area(self):
+        """
+        Area of BaseGeometry
+        """
+        raise Exception("area() is not implemented")
