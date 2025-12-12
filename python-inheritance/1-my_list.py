78851add023c10b1a2101c42f509b717d26a7cbf
mammadow <mammadow.elnur@gmail.com>
2025-12-12 15:28:03 +0000

    fix

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -11,6 +11,6 @@ class MyList(list):
 
     def print_sorted(self):
         """
-        Prints the list sorted in ascending order without modifying the original list.
+        Prints the list sorted in ascending order.
         """
         print(sorted(self))
