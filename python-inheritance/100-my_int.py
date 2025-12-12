c9917feeffd8bf45f79b775bc397453f725588f9
mammadow <mammadow.elnur@gmail.com>
2025-12-12 16:55:13 +0000

    add MyInt

---
+++ python-inheritance/100-my_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Defines a MyInt class that inverts == and != operators.
+"""
+
+class MyInt(int):
+    """
+    Rebel integer class that inverts equality operators.
+    """
+
+    def __eq__(self, other):
+        """
+        Inverts == operator.
+        """
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """
+        Inverts != operator.
+        """
+        return super().__eq__(other)
