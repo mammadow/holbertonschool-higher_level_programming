2892b92426bfdbf9803525dd199488d98cc7b9cd
mammadow <mammadow.elnur@gmail.com>
2025-12-12 12:32:42 +0000

    add nqueens

---
+++ python-more_classes/101-nqueens.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+import sys
+
+
+def print_usage_and_exit(msg):
+    print(msg)
+    sys.exit(1)
+
+
+def is_safe(board, row, col):
+    for r, c in board:
+        if c == col or abs(r - row) == abs(c - col):
+            return False
+    return True
+
+
+def solve_nqueens(n, row, board):
+    if row == n:
+        print(board)
+        return
+
+    for col in range(n):
+        if is_safe(board, row, col):
+            solve_nqueens(n, row + 1, board + [[row, col]])
+
+
+def main():
+    if len(sys.argv) != 2:
+        print_usage_and_exit("Usage: nqueens N")
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print_usage_and_exit("N must be a number")
+
+    if n < 4:
+        print_usage_and_exit("N must be at least 4")
+
+    solve_nqueens(n, 0, [])
+
+if __name__ == "__main__":
+    main()
