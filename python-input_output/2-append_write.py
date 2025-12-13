ca03d86192b16f3331eac9636fdc7eef80914b38
mammadow <mammadow.elnur@gmail.com>
2025-12-13 17:11:57 +0000

    add append

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module to define a function that appends string to a text file."""
+
+
+def append_write(filename="", text=""):
+    """Defines a function that appends to text file."""
+    with open(filename, mode="a", encoding="utf-8") as f:
+        f.write(text)
