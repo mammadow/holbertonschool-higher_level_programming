80036ee1a2f617daf5764fd7974a996f1e5638a5
mammadow <mammadow.elnur@gmail.com>
2025-12-14 10:15:02 +0000

    add json string

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that defines a function to return the JSON representation of an object."""
+import json
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation (string) of a Python object."""
+    return json.dumps(my_obj)
