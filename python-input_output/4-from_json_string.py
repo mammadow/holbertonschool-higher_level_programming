d3e4987f768071e90a36e0d6b4f229e8e54401ad
mammadow <mammadow.elnur@gmail.com>
2025-12-14 10:17:09 +0000

    add from json string

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module that defines a function to convert a JSON string to a Python object.
+"""
+import json
+
+
+def from_json_string(my_str):
+    """Returns a Python object represented by a JSON string."""
+    return json.loads(my_str)
