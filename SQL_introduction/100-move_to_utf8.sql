bdfe324cdd106fc60afd725f366db8b61c71b2e0
mammadow <mammadow.elnur@gmail.com>
2025-12-20 19:25:22 +0000

    fix

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,4 @@
--- Convert hbtn_0c_0 database, table, and name column to utf8mb4
+-- Convert hbtn_0c_0 database and first_table to utf8mb4
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE hbtn_0c_0;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table CHANGE name name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
