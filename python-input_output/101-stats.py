011c2e987cddea8873e4776e664d9d8ea1851e77
mammadow <mammadow.elnur@gmail.com>
2025-12-20 15:26:48 +0000

    stats

---
+++ python-input_output/101-stats.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+This script reads stdin line by line and computes metrics
+"""
+
+import sys
+
+
+def print_stats(total_size, status_counts):
+    print("File size: {}".format(total_size))
+    for status in sorted(status_counts.keys()):
+        if status_counts[status] > 0:
+            print("{}: {}".format(status, status_counts[status]))
+
+total_size = 0
+line_count = 0
+status_counts = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
+
+try:
+    for line in sys.stdin:
+        parts = line.split()
+
+        if len(parts) >= 2:
+            status = parts[-2]
+            size = parts[-1]
+
+            if status in status_counts:
+                status_counts[status] += 1
+
+            try:
+                total_size += int(size)
+            except Exception:
+                pass
+
+        line_count += 1
+
+        if line_count % 10 == 0:
+            print_stats(total_size, status_counts)
+
+except KeyboardInterrupt:
+    print_stats(total_size, status_counts)
+    raise
