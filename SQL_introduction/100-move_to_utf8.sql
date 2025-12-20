7ec33653d30c94b2489a0880ef59a6a8001b5b04
mammadow <mammadow.elnur@gmail.com>
2025-12-20 19:17:35 +0000

    move to utf8

---
+++ SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,13 @@
+-- Convert the database hbtn_0c_0 to UTF8
+ALTER DATABASE hbtn_0c_0
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+-- Convert the table first_table to UTF8
+ALTER TABLE first_table
+CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+-- Convert the name field in first_table to UTF8
+ALTER TABLE first_table
+MODIFY name VARCHAR(256)
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
