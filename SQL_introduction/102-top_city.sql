a1bd56e2d4fb03531f4463e39884de7baaf56a57
mammadow <mammadow.elnur@gmail.com>
2025-12-20 19:27:15 +0000

    add top city

---
+++ SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- Display top 3 cities average temperature in July and August
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+WHERE month IN (7, 8)
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
