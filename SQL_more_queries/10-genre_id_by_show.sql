51ae73f8c174ed6f7036544e8c85b9cdf92c9d22
mammadow <mammadow.elnur@gmail.com>
2025-12-21 09:22:03 +0000

    fix

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,3 @@
 -- Genre ID by show
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_show.title ASC, tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
