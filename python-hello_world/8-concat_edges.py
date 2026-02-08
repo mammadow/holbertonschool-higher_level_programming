1c8ea8825814875a235f7fc08e6c92a27122ee54
mammadow <88704613+mammadow@users.noreply.github.com>
2026-02-08 16:11:11 +0400

    Update 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-str = "Python object-oriented programming with "
-print(str[7:35] + str[35:41] + str[:6])
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
